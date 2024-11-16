b3a1dd0ad58b350dd5efaa91b0e005d30005dfbd
yassinalmach <yassineelmach0@gmail.com>
2024-11-16 16:21:39 +0100

    routing

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"groupie/handlers"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	http.HandleFunc("GET /", handlers.HomeHandler)
+	http.HandleFunc("GET /artist/", handlers.ArtistHandler)
+	http.HandleFunc("GET /static/", handlers.ServeStaticFiles)
+
+	http.ListenAndServe(":8080", mux)
+	fmt.Println("server running on: http://localhost:8080")
+}
