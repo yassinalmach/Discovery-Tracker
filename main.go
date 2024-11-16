10a6f3d47d1e39bf20397dea6c48491dc140d0de
yassinalmach <yassineelmach0@gmail.com>
2024-11-16 18:04:08 +0100

    add not found error page

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	http.HandleFunc("GET /", handlers.HomeHandler)
-	http.HandleFunc("GET /artist/", handlers.ArtistHandler)
-	http.HandleFunc("GET /static/", handlers.ServeStaticFiles)
+	mux.HandleFunc("GET /", handlers.HomeHandler)
+	mux.HandleFunc("GET /artist/", handlers.ArtistHandler)
+	mux.HandleFunc("GET /static/", handlers.ServeStaticFiles)
 
-	http.ListenAndServe(":8080", mux)
 	fmt.Println("server running on: http://localhost:8080")
+	http.ListenAndServe(":8080", mux)
 }
--- utils/pageNotFound.go
+++ utils/pageNotFound.go
@@ -1,7 +1,20 @@
 package utils
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
-func PageNotFound(w http.ResponseWriter, r *http.Request, code int) {
-	
+func PageNotFound(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("./templates/notFoundPage.html")
+	if err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	if err := tmp.Execute(w, "Error 404 (Not Found)!!"); err != nil {
+		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
