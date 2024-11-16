badacdbf58bbae5d675840c5b96855800b72644e
yassinalmach <yassineelmach0@gmail.com>
2024-11-16 16:23:36 +0100

    add models and fetch func

--- api/fetchData.go
+++ api/fetchData.go
@@ -1,1 +1,15 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func FetchData(w http.ResponseWriter, url string, data any) {
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "request failed", 500)
+	}
+	defer resp.Body.Close()
+	json.NewDecoder(resp.Body).Decode(data)
+}
--- api/models.go
+++ api/models.go
@@ -1,1 +1,24 @@
 package api
+
+type ArtistType struct {
+	Id           int
+	Name         string
+	Image        string
+	Members      []string
+	CreationDate int
+	FirstAlbum   string
+	Location     LocationType
+	Date         DateType
+	Relation     RelationType
+}
+
+type LocationType struct {
+	Location []string
+}
+
+type DateType struct {
+	Date []string
+}
+type RelationType struct {
+	Relation map[string][]string
+}
